99015919e60f94bd48dbe66613a1605092afb970
akpax <austin.paxton00@gmail.com>
2023-12-16 14:01:04 -0800

    put script inside if name=main

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ from datetime import datetime
 import matplotlib as plt 
 import tkinter as tk
 
-###################################### tkinter entry for demo #################################################
-window = tk.Tk()
-tk.Label(window, text="Excel Path").grid(row=0)
-tk.Label(window, text="Cell Range").grid(row=1)
-tk.Label(window, text="Specify Desired Clusters (optional)").grid(row=2)
+# ###################################### tkinter entry for demo #################################################
+# window = tk.Tk()
+# tk.Label(window, text="Excel Path").grid(row=0)
+# tk.Label(window, text="Cell Range").grid(row=1)
+# tk.Label(window, text="Specify Desired Clusters (optional)").grid(row=2)
 
-excel_path = tk.Entry(window)
-cell_range = tk.Entry(window)
-cluster_qty = tk.Entry(window)
+# excel_path = tk.Entry(window)
+# cell_range = tk.Entry(window)
+# cluster_qty = tk.Entry(window)
 
-excel_path.grid(row=0, column=1)
-cell_range.grid(row=1, column=1)
-cluster_qty.grid(row=2, column=1)
+# excel_path.grid(row=0, column=1)
+# cell_range.grid(row=1, column=1)
+# cluster_qty.grid(row=2, column=1)
 
-tk.Button(window, text="Analyze", command=window.quit).grid(row=3, column=1)
+# tk.Button(window, text="Analyze", command=window.quit).grid(row=3, column=1)
 
-tk.mainloop()
+# tk.mainloop()
 
-#get inputs from tkinter
-file_path = excel_path.get()
-user_input = cell_range.get()
-user_cluster_qty = cluster_qty.get()
-print(type(user_cluster_qty))
+# #get inputs from tkinter
+# file_path = excel_path.get()
+# user_input = cell_range.get()
+# user_cluster_qty = cluster_qty.get()
+# print(type(user_cluster_qty))
 
 ###################################### Inputs #################################################
-# file_path = "/Users/austinpaxton/Documents/ML_projects/LoadClusterScheduler/test_files/Pier Loads.xlsx"
-# user_input = "Pier Load Summary!A29:D50"
-# user_cluster_qty= None
+file_path = "/Users/austinpaxton/Documents/ML_projects/LoadClusterScheduler/test_files/Pier Loads.xlsx"
+user_input = "Pier Load Summary!A29:C50"
+user_cluster_qty= 3
 
 ###################################### Functions #################################################
 def format_data_for_training(X):
@@ -65,49 +65,49 @@ def create_output_folder_path(name):
     output_folder_path = os.path.join(home_directory,"Desktop",folder_name)
     return output_folder_path
 
+if __name__ == "__main__":
+    parser = ExcelInputParser()
+    parser.parse_input(user_input)
 
-parser = ExcelInputParser()
-parser.parse_input(user_input)
+    # Extract necessary arguments for pd.read_excel
+    sheet_name = parser.sheet_name
+    usecols = parser.extract_usecols()
+    skiprows, nrows = parser.extract_rows()
 
-# Extract necessary arguments for pd.read_excel
-sheet_name = parser.sheet_name
-usecols = parser.extract_usecols()
-skiprows, nrows = parser.extract_rows()
+    df = pd.read_excel(file_path, sheet_name=sheet_name, usecols=usecols, skiprows=skiprows, nrows=nrows)
+    print(df.tail())
 
-df = pd.read_excel(file_path, sheet_name=sheet_name, usecols=usecols, skiprows=skiprows, nrows=nrows)
-print(df.tail())
+        
+    X = format_data_for_training(df)
 
-    
-X = format_data_for_training(df)
-
-print(X.head())
-# pick model based on wether user specified cluster quantity
-if not user_cluster_qty:
-    model = MeanShift()
-    print("MODEL: MeanShift")
-else:
-    model = KMeans(int(user_cluster_qty))
-    print("MODEL: KMeans")
-    
-# fit model and update user_input dataframe
-model.fit(X)
-labels = model.labels_
-df["labels"] = model.labels_
-df = df.sort_values(by="labels",ascending=True)
-centers = model.cluster_centers_
+    print(X.head())
+    # pick model based on wether user specified cluster quantity
+    if not user_cluster_qty:
+        model = MeanShift()
+        print("MODEL: MeanShift")
+    else:
+        model = KMeans(int(user_cluster_qty))
+        print("MODEL: KMeans")
+        
+    # fit model and update user_input dataframe
+    model.fit(X)
+    labels = model.labels_
+    df["labels"] = model.labels_
+    df = df.sort_values(by="labels",ascending=True)
+    centers = model.cluster_centers_
 
 
-output_folder_path = create_output_folder_path("cluster_report")
+    output_folder_path = create_output_folder_path("cluster_report")
 
-if not os.path.exists(output_folder_path):
-    os.makedirs(output_folder_path)
-    
-# output user dataframe w results to .csv in report folder
-csv_path = os.path.join(output_folder_path, "user_input&results.csv")
-df.to_csv(csv_path, index=False)
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+        
+    # output user dataframe w results to .csv in report folder
+    csv_path = os.path.join(output_folder_path, "user_input&results.csv")
+    df.to_csv(csv_path, index=False)
 
-# plot results and output to output folder
-cluster_visualizer = ClusterVisualizer()
-cluster_visualizer.set_data(data=X, labels=labels, centers=centers)
-cluster_visualizer.plot(output_folder_path)
+    # plot results and output to output folder
+    cluster_visualizer = ClusterVisualizer()
+    cluster_visualizer.set_data(data=X, labels=labels, centers=centers)
+    cluster_visualizer.plot(output_folder_path)
 
